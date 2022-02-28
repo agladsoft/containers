3e9f3aea3d955abb7d332ea9d29456c7c576dd89
Timur <timurka.zavyalov@mail.ru>
2022-02-28 13:51:40 +0300

    init_one

--- scripts/statistics_2021.py
+++ scripts/statistics_2021.py
@@ -5,9 +5,12 @@ import re
 import sys
 from collections import defaultdict
 
+month_list = ["ЯНВАРЬ", "ФЕВРАЛЬ", "МАРТ", "АПРЕЛЬ", "МАЙ", "ИЮНЬ", "ИЮЛЬ", "АВГУСТ", "СЕНТЯБРЬ", "ОКТЯБРЬ", "НОЯБРЬ",
+              "ДЕКАБРЬ"]
+
 columns = defaultdict(list)  # each value in each column is appended to a list
 
-with open(os.path.abspath(sys.argv[1])) as f:
+with open("/home/timur/PycharmWork/containers/statistics_2021/csv/СТАТИСТИКА_2021.xls.csv") as f:
     reader = csv.DictReader(f)  # read rows into a dictionary format
     for row in reader:  # read a row as {column1: value1, column2: value2,...}
         for (k, v) in row.items():  # go over each column name and value
@@ -15,54 +18,25 @@ with open(os.path.abspath(sys.argv[1])) as f:
 
 
 def merge_two_dicts(x, y):
-    z = x.copy()   # start with keys and values of x
-    z.update(y)    # modifies z with keys and values of y
+    z = x.copy()  # start with keys and values of x
+    z.update(y)  # modifies z with keys and values of y
     return z
 
 
-def parse_column(parsed_data, enum, column0, column1, column2, column3, column4, column5, column6, column7, column8):
-    ship = {}
-    import_name = defaultdict(list)
-    direct = defaultdict(list)
-    type = defaultdict(list)
-    count = defaultdict(list)
-    line = defaultdict(list)
-
-    ship['ship_name'] = columns[column1][enum]
-    import_name['import'].append(columns[column1][enum + 1])
-    import_name['export'].append(columns[column5][enum + 1])
-    direct['loaded'].append(columns[column1][enum + 2])
-    direct['empty'].append(columns[column3][enum + 2])
-
-    direct['loaded'].append(columns[column5][enum + 2])
-    direct['empty'].append(columns[column7][enum + 2])
-
-    type['container_type'].append(columns[column1][enum + 3])
-    type['container_type'].append(columns[column2][enum + 3])
-    type['container_type'].append(columns[column3][enum + 3])
-    type['container_type'].append(columns[column4][enum + 3])
-
-    type['container_type'].append(columns[column5][enum + 3])
-    type['container_type'].append(columns[column6][enum + 3])
-    type['container_type'].append(columns[column7][enum + 3])
-    type['container_type'].append(columns[column8][enum + 3])
-
-    list_index = [i + 5 for i, item in enumerate(columns[column1][enum + 5:enum + 19]) if re.search('\d', item)]
-    # print(list_index)
-
-    for enum_for_value in list_index:
-        count['count'].append(columns[column1][enum + enum_for_value])
-        count['count'].append(columns[column2][enum + enum_for_value])
-        count['count'].append(columns[column3][enum + enum_for_value])
-        count['count'].append(columns[column4][enum + enum_for_value])
-        count['count'].append(columns[column5][enum + enum_for_value])
-        count['count'].append(columns[column6][enum + enum_for_value])
-        count['count'].append(columns[column7][enum + enum_for_value])
-        count['count'].append(columns[column8][enum + enum_for_value])
-
-        line['line'].append(columns[column0][enum + enum_for_value])
-
-    x = {**ship, **import_name, **direct, **line, **type, **count}
+def parse_column(parsed_data, enum, column0, column1, enum_for_value):
+    type = dict()
+    count = dict()
+    line = dict()
+    context['ship_name'] = context['ship_name'] if not columns[column1][enum] else \
+        re.findall(r'\b\S+\b', columns[column1][enum])[1]
+    context['direction'] = context['direction'] if not columns[column1][enum + 1] else \
+        "import" if columns[column1][enum + 1] == 'выгрузка' else "export"
+    context['is_empty'] = context['is_empty'] if not columns[column1][enum + 2] else \
+        False if columns[column1][enum + 2] == 'груженые' else True
+    type['container_size'] = int("".join(re.findall("\d", columns[column1][enum + 3]))[:2])
+    count['count'] = int(float(columns[column1][enum + enum_for_value]))
+    line['line'] = columns[column0][enum + enum_for_value].rsplit('/', 1)[0]
+    x = {**line, **type, **count}
     record = merge_two_dicts(context, x)
     parsed_data.append(record)
 
@@ -73,7 +47,6 @@ context = dict()
 
 def process(input_file_path):
     columns = defaultdict(list)  # each value in each column is appended to a list
-
     with open(input_file_path) as file:
         reader = csv.DictReader(file)  # read rows into a dictionary format
         for row in reader:  # read a row as {column1:Линия/Агент value1, column2: value2,...}
@@ -81,27 +54,22 @@ def process(input_file_path):
                 columns[key].append(value)
     zip_list = list(columns)
     print(zip_list)
-    context['month'] = zip_list[0]
-    for enum, (ship_name, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y) in \
-            enumerate(zip(columns[zip_list[0]], columns[zip_list[1]], columns[zip_list[2]], columns[zip_list[3]],
-            columns[zip_list[4]], columns[zip_list[5]], columns[zip_list[6]], columns[zip_list[7]], columns[zip_list[8]],
-            columns[zip_list[9]], columns[zip_list[10]], columns[zip_list[11]], columns[zip_list[12]], columns[zip_list[13]],
-            columns[zip_list[14]], columns[zip_list[15]], columns[zip_list[16]],
-            columns[zip_list[17]], columns[zip_list[18]], columns[zip_list[19]], columns[zip_list[20]],
-            columns[zip_list[21]], columns[zip_list[22]], columns[zip_list[23]], columns[zip_list[24]])):
+    month = zip_list[0].rsplit(' ', 1)
+    if month[0] in month_list:
+        month_digit = month_list.index(month[0]) + 1
+    context['month'] = month_digit
+    context['year'] = int(month[1])
+    for enum, ship_name in enumerate(columns[zip_list[0]]):
         if ship_name == 'Название судна':
-            parse_column(parsed_data, enum, zip_list[0], zip_list[1], zip_list[2], zip_list[3], zip_list[4],
-                         zip_list[5], zip_list[6], zip_list[7], zip_list[8])
-            parse_column(parsed_data, enum, zip_list[0], zip_list[9], zip_list[10], zip_list[11], zip_list[12],
-                         zip_list[13], zip_list[14], zip_list[15], zip_list[16])
-            parse_column(parsed_data, enum, zip_list[0], zip_list[17], zip_list[18], zip_list[19], zip_list[20],
-                         zip_list[21], zip_list[22], zip_list[23], zip_list[24])
+            for column in zip_list:
+                list_index = [i + 5 for i, item in enumerate(columns[column][enum + 5:enum + 19]) if re.search('\d',
+                                                                                                               item)]
+                for enum_for_value in list_index:
+                    parse_column(parsed_data, enum, zip_list[0], column, enum_for_value)
 
     return parsed_data
 
 
-# input_file_path = "/home/timur/PycharmWork/containers/17.02/csv/СТАТИСТИКА_2021.xls.csv"
-# output_folder = "/home/timur/PycharmWork/containers/17.02/json/"
 input_file_path = os.path.abspath(sys.argv[1])
 output_folder = sys.argv[2]
 basename = os.path.basename(input_file_path)
